50e1b20f835fa0c85222b86cd669fdd7746923c6
Sergio-rsd <sergsa2001@mail.ru>
2022-02-20 18:24:30 +0300

    Новое окно вывода погоды города - улучшения

---
+++ app/src/main/java/ru/gb/kotlinapp/util/utilities.kt
@@ -0,0 +1,11 @@
+package ru.gb.kotlinapp.util
+
+import java.time.Duration
+import java.time.LocalTime
+
+fun longitudeSunDay(sunrise: String, sunset: String): String {
+    val duration = Duration.between(LocalTime.parse(sunrise), LocalTime.parse(sunset))
+    val hour = duration.toMinutes() / 60
+    val minute = duration.toMinutes() % 60
+    return String.format("%02d:%02d", hour, minute)
+}
--- app/src/main/java/ru/gb/kotlinapp/view/details/DetailsFragment.kt
+++ app/src/main/java/ru/gb/kotlinapp/view/details/DetailsFragment.kt
@@ -1,6 +1,8 @@
 package ru.gb.kotlinapp.view.details
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,10 +10,14 @@ import androidx.fragment.app.Fragment
 import ru.gb.kotlinapp.R
 import ru.gb.kotlinapp.databinding.FragmentWeatherCityBinding
 import ru.gb.kotlinapp.model.Weather
+import ru.gb.kotlinapp.util.longitudeSunDay
+import java.text.SimpleDateFormat
+import java.util.*
 
 class DetailsFragment : Fragment() {
     private var _binding: FragmentWeatherCityBinding? = null
     private val binding get() = _binding!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,6 +28,7 @@ class DetailsFragment : Fragment() {
         return binding.root
     }
 
+    @SuppressLint("SimpleDateFormat")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -29,6 +36,16 @@ class DetailsFragment : Fragment() {
 
         if (weather != null) {
             val city = weather.city
+            object : CountDownTimer(2000, 500) {
+                override fun onTick(millisUntilFinished: Long) {
+                    binding.loadingLayout.visibility = View.VISIBLE
+                }
+
+                override fun onFinish() {
+                    binding.loadingLayout.visibility = View.GONE
+                }
+            }.start()
+
             binding.cityName.text = city.city
             binding.timeAfterLastUpdate.text = String.format(
                 getString(R.string.time_after_last_update),
@@ -37,19 +54,52 @@ class DetailsFragment : Fragment() {
             )
             binding.currentTimeData.text = String.format(
                 getString(R.string.current_time_data),
-                "",
-                "",
-                ""
+                SimpleDateFormat("HH:mm, EEEE, d MMMM").format(Calendar.getInstance().time),
+
             )
             binding.temperatureValue.text = String.format(
                 getString(R.string.current_weather_value),
                 plusMinusTemperature(weather.temperature),
-                ""
+                weather.weatherCondition
             )
             binding.feelsLikeValue.text = String.format(
                 getString(R.string.feels_like_label_value),
                 plusMinusTemperature(weather.feelsLike)
             )
+            binding.precipitationStrengthValue.text = String.format(
+                getString(R.string.precipitation_strength_value),
+                weather.precipitationStrength.toString()
+            )
+            binding.pressureMmValue.text=String.format(
+                getString(R.string.pressure_mm_value),
+                    weather.pressure.toString(),
+                    weather.pressureNormal.toString()
+            )
+            binding.humidityValue.text = String.format(
+                getString(R.string.humidity_value),
+                weather.humidity.toString(),
+                '%'
+            )
+            binding.windValue.text = String.format(
+                getString(R.string.wind_value),
+                weather.wind.toString(),
+                weather.windDirection
+            )
+            binding.cloudinessValue.text = String.format(
+                getString(R.string.cloudiness_value),
+                ((weather.cloudiness * 100).toInt()).toString(),
+                '%'
+            )
+            binding.sunDayValue.text = String.format(
+                getString(R.string.sun_day_value),
+                weather.sunrise,
+                weather.sunset
+            )
+            binding.longitudeDayValue.text = String.format(
+                getString(R.string.longitude_day_value),
+                longitudeSunDay(weather.sunrise,weather.sunset)
+            )
+            binding.moonValue.text = weather.moon
         }
     }
 
