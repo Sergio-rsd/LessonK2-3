44ec35787583e05fe6682216967f5a71fa6c20c5
Sergio-rsd <sergsa2001@mail.ru>
2022-03-15 20:16:33 +0300

    ДЗ 5 все работает. Осталась обработка ошибок

--- app/src/main/java/ru/gb/kotlinapp/util/Utilities.kt
+++ app/src/main/java/ru/gb/kotlinapp/util/Utilities.kt
@@ -5,15 +5,19 @@ import com.google.android.material.snackbar.Snackbar
 import java.time.Duration
 import java.time.LocalTime
 
-fun longitudeSunDay(sunrise: String, sunset: String): String {
+fun longitudeSunDay(sunrise: String?, sunset: String?): String? {
     val duration = Duration.between(LocalTime.parse(sunrise), LocalTime.parse(sunset))
     val hour = duration.toMinutes() / 60
     val minute = duration.toMinutes() % 60
     return String.format("%02d:%02d", hour, minute)
 }
 
-fun plusMinusTemperature(temp: Int): String {
-    return if (temp > 0) "+$temp" else temp.toString()
+fun plusMinusTemperature(temp: Int?): String {
+    return if (temp ?: 404 > 0) "+$temp" else temp.toString()
+}
+
+fun cloudPercent(cloud: Double?): String {
+    return (cloud?.times(100) ?: 404).toInt().toString()
 }
 
 // random access
@@ -29,6 +33,7 @@ fun View.showSnackBar(
 ) {
     Snackbar.make(this, text, length).setAction(actionText, action).show()
 }
+
 // ДЗ 4-2
 fun View.showSnackBarNoAction(
     text: String,
@@ -40,6 +45,7 @@ fun View.showSnackBarNoAction(
         .setAction(actionText, null)
         .show()
 }
+
 fun View.showSnackBarStringText(
     text: Int,
     actionText: Int,
--- app/src/main/java/ru/gb/kotlinapp/view/details/DetailsFragment.kt
+++ app/src/main/java/ru/gb/kotlinapp/view/details/DetailsFragment.kt
@@ -2,19 +2,20 @@ package ru.gb.kotlinapp.view.details
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.os.CountDownTimer
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import ru.gb.kotlinapp.R
 import ru.gb.kotlinapp.databinding.FragmentWeatherCityBinding
-import ru.gb.kotlinapp.model.Weather
-import ru.gb.kotlinapp.model.WeatherDTO
+import ru.gb.kotlinapp.model.*
+import ru.gb.kotlinapp.util.cloudPercent
 import ru.gb.kotlinapp.util.longitudeSunDay
 import ru.gb.kotlinapp.util.plusMinusTemperature
 import java.text.SimpleDateFormat
 import java.util.*
+
+private const val TAG = "happy"
 
 class DetailsFragment : Fragment(R.layout.main_fragment) {
     private var _binding: FragmentWeatherCityBinding? = null
@@ -22,15 +23,17 @@ class DetailsFragment : Fragment(R.layout.main_fragment) {
 
     private lateinit var weatherBundle: Weather
 
-    private val onLoadListener: WeatherLoader.WeatherLoaderListener = object : WeatherLoader.WeatherLoaderListener {
-        override fun onLoaded(weatherDTO: WeatherDTO) {
-            displayWeather(weatherDTO)
-        }
 
-        override fun onFailed(throwable: Throwable) {
-           // TODO обработка ошибки
+    private val onLoadListener: WeatherLoader.WeatherLoaderListener =
+        object : WeatherLoader.WeatherLoaderListener {
+            override fun onLoaded(weatherDTO: WeatherDTO) {
+                displayWeather(weatherDTO)
+            }
+
+            override fun onFailed(throwable: Throwable) {
+                // TODO обработка ошибки
+            }
         }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -45,39 +48,43 @@ class DetailsFragment : Fragment(R.layout.main_fragment) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        weatherBundle = arguments?.getParcelable<Weather>(BUNDLE_EXTRA)?:Weather()
+        weatherBundle = arguments?.getParcelable<Weather>(BUNDLE_EXTRA) ?: Weather()
 
-        arguments?.getParcelable<Weather>(BUNDLE_EXTRA)?.let {
-            it.city.also { city ->
-                binding.cityName.text = city.city
-            }
-            with(binding){
-                timeAfterLastUpdate.text = String.format(
-                    getString(R.string.time_after_last_update),
-                    "",
-                    ""
-                )
-                currentTimeData.text = String.format(
-                    getString(R.string.current_time_data),
-                    SimpleDateFormat("HH:mm, EEEE, d MMMM").format(Calendar.getInstance().time)
-                )
+        binding.mainViewWeather.visibility = View.GONE
+        binding.loadingLayout.visibility = View.VISIBLE
+
+        val loader = WeatherLoader(onLoadListener, weatherBundle.city.lat, weatherBundle.city.lon)
+
+        loader.loadWeather()
+
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    private fun displayWeather(weatherDTO: WeatherDTO) {
+        with(binding) {
+            mainViewWeather.visibility = View.VISIBLE
+            loadingLayout.visibility = View.GONE
+            val city = weatherBundle.city
+            cityName.text = city.city
+
+            currentTimeData.text = String.format(
+                getString(R.string.current_time_data),
+                SimpleDateFormat("HH:mm, EEEE, d MMMM").format(Calendar.getInstance().time)
+            )
+            weatherDTO.fact?.let {
                 temperatureValue.text = String.format(
                     getString(R.string.current_weather_value),
-                    plusMinusTemperature(it.temperature),
-                    it.weatherCondition
+                    plusMinusTemperature(it.temp),
+                    getCondition()[it.condition]
                 )
                 feelsLikeValue.text = String.format(
                     getString(R.string.feels_like_label_value),
-                    plusMinusTemperature(it.feelsLike)
-                )
-                precipitationStrengthValue.text = String.format(
-                    getString(R.string.precipitation_strength_value),
-                    it.precipitationStrength.toString()
+                    plusMinusTemperature(it.feels_like)
                 )
                 pressureMmValue.text = String.format(
                     getString(R.string.pressure_mm_value),
-                    it.pressure.toString(),
-                    it.pressureNormal.toString()
+                    it.pressure_mm.toString(),
+                    weatherDTO.info?.def_pressure_mm.toString()
                 )
                 humidityValue.text = String.format(
                     getString(R.string.humidity_value),
@@ -86,14 +93,17 @@ class DetailsFragment : Fragment(R.layout.main_fragment) {
                 )
                 windValue.text = String.format(
                     getString(R.string.wind_value),
-                    it.wind.toString(),
-                    it.windDirection
+                    it.wind_speed.toString(),
+                    getWindDirection()[it.wind_dir]
                 )
                 cloudinessValue.text = String.format(
                     getString(R.string.cloudiness_value),
-                    ((it.cloudiness * 100).toInt()).toString(),
+                    cloudPercent(it.cloudness),
                     '%'
                 )
+            }
+
+            weatherDTO.forecasts[0]?.let {
                 sunDayValue.text = String.format(
                     getString(R.string.sun_day_value),
                     it.sunrise,
@@ -103,25 +113,13 @@ class DetailsFragment : Fragment(R.layout.main_fragment) {
                     getString(R.string.longitude_day_value),
                     longitudeSunDay(it.sunrise, it.sunset)
                 )
-                moonValue.text = it.moon
+                precipitationStrengthValue.text = String.format(
+                    getString(R.string.precipitation_strength_value),
+                    it.parts?.day?.prec_mm.toString()
+                )
+                moonValue.text = getMoonCondition()[it.moon_text]
             }
-/*
-* imitations of time delay
-* */
-            object : CountDownTimer(2000, 500) {
-                override fun onTick(millisUntilFinished: Long) {
-                    binding.loadingLayout.visibility = View.VISIBLE
-                }
-
-                override fun onFinish() {
-                    binding.loadingLayout.visibility = View.GONE
-                }
-            }.start()
         }
-    }
-
-    private fun displayWeather(weatherDTO: WeatherDTO){
-
     }
 
     override fun onDestroyView() {
